36440dec54613516e4f5ea5925007c584ac1d42f
Shrey Banga <banga.shrey@gmail.com>
2021-04-18 00:53:36 -0700

    Update main.yml

--- scripts/screenshotTheme.sh
+++ scripts/screenshotTheme.sh
@@ -1,21 +1,15 @@
 git config core.pager "./bin/git-split-diffs --color"
 git config split-diffs.theme-name "$1"
 
-# CWD=$(pwd)
-# SCRIPT='
-# tell app "Finder"
-#     set b to bounds of window of desktop
-# end tell
-# tell app "Terminal"
-#     activate
-#     do script"'
-# SCRIPT+="cd $CWD; git show"
-# SCRIPT+='"
-#     set the bounds of the front window to b
-# end tell
-# '
-# osascript -e "$SCRIPT"
-
-open -a Terminal
+CWD=$(pwd)
+SCRIPT='
+tell app "Terminal"
+    activate
+    do script"'
+SCRIPT+="cd $CWD; git show"
+SCRIPT+='"
+end tell
+'
+osascript -e "$SCRIPT"
 
 screencapture -x -T 1 "screenshots/$1.png"
