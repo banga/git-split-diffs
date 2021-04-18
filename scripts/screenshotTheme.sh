a7a720c64cedcdc789c1beed7e3c85bf95d5029e
Shrey Banga <banga.shrey@gmail.com>
2021-04-18 00:35:42 -0700

    Script to take screenshots

---
+++ scripts/screenshotTheme.sh
@@ -0,0 +1,19 @@
+git config core.pager "./bin/git-split-diffs --color"
+git config split-diffs.theme-name "$1"
+
+CWD=$(pwd)
+SCRIPT='
+tell app "Finder"
+    set b to bounds of window of desktop
+end tell
+tell app "Terminal"
+    activate
+    do script"'
+SCRIPT+="cd $CWD; git show"
+SCRIPT+='"
+    set the bounds of the front window to b
+end tell
+'
+osascript -e "$SCRIPT"
+
+screencapture -x -T 1 "screenshots/$1.png"
