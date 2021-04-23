3033e810cb2aeb1de25ea670dc986a2ce5809a50
Shrey Banga <banga.shrey@gmail.com>
2021-04-22 17:05:06 -0700

    Add SpannedString
    This will help make syntax highlighting, inline change highlighting and line
    wrapping easier. It has a small overhead which can probably be improved.

---
+++ src/SpannedString.test.ts
@@ -0,0 +1,244 @@
+import { SpannedString } from './SpannedString';
+
+function S() {
+    return SpannedString.create<string>();
+}
+
+function apply(string: SpannedString<string>) {
+    return Array.from(string.iterSubstrings())
+        .map(([substring, tags]) => {
+            let s = substring;
+            for (const tag of tags) {
+                s = `<${tag}>${s}</${tag}>`;
+            }
+            return s;
+        })
+        .join('');
+}
+
+function format(string: string, spans: [number, number, string][]) {
+    const s = S().appendString(string);
+    for (const span of spans) {
+        s.addSpan(...span);
+    }
+    return apply(s);
+}
+
+test('no spans', () => {
+    expect(format('one two', [])).toEqual('one two');
+});
+
+test('single span', () => {
+    expect(format('one two', [[0, 3, 'b']])).toEqual('<b>one</b> two');
+
+    expect(format('one two', [[3, 4, 'b']])).toEqual('one<b> </b>two');
+
+    expect(format('one two', [[4, 7, 'b']])).toEqual('one <b>two</b>');
+});
+
+test('non-overlapping spans', () => {
+    expect(
+        format('one two', [
+            [0, 3, 'b'],
+            [4, 7, 'i'],
+        ])
+    ).toEqual('<b>one</b> <i>two</i>');
+
+    expect(
+        format('one two', [
+            [4, 7, 'i'],
+            [0, 3, 'b'],
+        ])
+    ).toEqual('<b>one</b> <i>two</i>');
+});
+
+test('adjacent spans', () => {
+    expect(
+        format('one two', [
+            [0, 4, 'b'],
+            [4, 7, 'i'],
+        ])
+    ).toEqual('<b>one </b><i>two</i>');
+});
+
+test('containing spans', () => {
+    expect(
+        format('one two', [
+            [2, 4, 'b'],
+            [0, 7, 'i'],
+        ])
+    ).toEqual('<i>on</i><i><b>e </b></i><i>two</i>');
+
+    expect(
+        format('one two three', [
+            [0, 3, 'i'],
+            [4, 7, 'b'],
+            [0, 13, 'u'],
+        ])
+    ).toEqual('<u><i>one</i></u><u> </u><u><b>two</b></u><u> three</u>');
+
+    expect(
+        format('one two', [
+            [1, 6, 'i'],
+            [2, 5, 'b'],
+            [3, 4, 'u'],
+        ])
+    ).toEqual(
+        'o<i>n</i><b><i>e</i></b><u><b><i> </i></b></u><b><i>t</i></b><i>w</i>o'
+    );
+});
+
+test('overlapping spans', () => {
+    expect(
+        format('one two', [
+            [0, 4, 'b'],
+            [3, 7, 'i'],
+        ])
+    ).toEqual('<b>one</b><i><b> </b></i><i>two</i>');
+
+    expect(
+        format('one two', [
+            [3, 7, 'i'],
+            [0, 4, 'b'],
+        ])
+    ).toEqual('<b>one</b><b><i> </i></b><i>two</i>');
+});
+
+test('order of application', () => {
+    expect(
+        format('one', [
+            [0, 3, 'b'],
+            [0, 3, 'i'],
+        ])
+    ).toEqual('<i><b>one</b></i>');
+
+    expect(
+        format('one', [
+            [0, 3, 'i'],
+            [0, 3, 'b'],
+        ])
+    ).toEqual('<b><i>one</i></b>');
+
+    expect(
+        format('one', [
+            [0, 3, 'b'],
+            [1, 3, 'i'],
+        ])
+    ).toEqual('<b>o</b><i><b>ne</b></i>');
+
+    expect(
+        format('one', [
+            [1, 3, 'i'],
+            [0, 3, 'b'],
+        ])
+    ).toEqual('<b>o</b><b><i>ne</i></b>');
+});
+
+test('construction', () => {
+    const string = S()
+        .appendString('one', 'b')
+        .appendString(' ')
+        .appendString('two', 'b', 'i')
+        .appendString(' ')
+        .appendString('three', 'u');
+
+    expect(Array.from(string.iterSubstrings())).toEqual([
+        ['one', ['b']],
+        [' ', []],
+        ['two', ['b', 'i']],
+        [' ', []],
+        ['three', ['u']],
+    ]);
+});
+
+describe('slice', () => {
+    test('containing spans', () => {
+        const string = S().appendString('one', 'b').addSpan(1, 3, 'i');
+        expect(apply(string)).toEqual('<b>o</b><i><b>ne</b></i>');
+
+        expect(apply(string.slice(1))).toEqual('<i><b>ne</b></i>');
+
+        expect(apply(string.slice(0, 1))).toEqual('<b>o</b>');
+
+        expect(apply(string.slice(1, 2))).toEqual('<i><b>n</b></i>');
+
+        expect(apply(string.slice(2, 3))).toEqual('<i><b>e</b></i>');
+    });
+
+    test('overlapping spans', () => {
+        const string = S()
+            .appendString('one two three')
+            .addSpan(0, 7, 'b')
+            .addSpan(4, 13, 'i');
+        expect(apply(string)).toEqual(
+            '<b>one </b><i><b>two</b></i><i> three</i>'
+        );
+
+        expect(apply(string.slice(4, 7))).toEqual('<i><b>two</b></i>');
+
+        expect(apply(string.slice(0, 3))).toEqual('<b>one</b>');
+
+        expect(apply(string.slice(8, 13))).toEqual('<i>three</i>');
+    });
+
+    test('empty span', () => {
+        const string = S().appendString('one', 'b');
+        expect(apply(string.slice(0, 0))).toEqual('');
+    });
+
+    test('out of bounds indexes', () => {
+        const a = S().appendString('one');
+
+        expect(apply(a.slice(0, 10))).toEqual('one');
+
+        expect(apply(a.slice(10, 10))).toEqual('');
+
+        expect(() => apply(a.slice(-1, 3))).toThrow();
+    });
+});
+
+describe('appendSpannedString', () => {
+    test('no spans', () => {
+        const s = S()
+            .appendString('one')
+            .appendSpannedString(S().appendString('two'));
+        expect(apply(s)).toEqual('onetwo');
+    });
+
+    test('one span', () => {
+        const a = S()
+            .appendString('one', 'b')
+            .appendSpannedString(S().appendString('two'));
+        expect(apply(a)).toEqual('<b>one</b>two');
+    });
+
+    test('adjacent spans', () => {
+        const a = S()
+            .appendString('one', 'b')
+            .appendSpannedString(S().appendString('two', 'i'));
+        expect(apply(a)).toEqual('<b>one</b><i>two</i>');
+    });
+
+    test('multiple spans', () => {
+        const a = S()
+            .appendString('one', 'b')
+            .addSpan(1, 2, 'u')
+            .appendSpannedString(
+                S().appendString('two', 'i').addSpan(2, 3, 'u')
+            );
+        expect(apply(a)).toEqual(
+            '<b>o</b><u><b>n</b></u><b>e</b><i>tw</i><u><i>o</i></u>'
+        );
+    });
+
+    test('modify concatenated string', () => {
+        const a = S().appendString('one', 'b');
+        const b = S().appendString('two', 'i');
+        a.appendSpannedString(b).addSpan(0, 6, 'u').addSpan(3, 6, 'em');
+        const c = a.slice(3, 6);
+
+        expect(apply(a)).toEqual('<u><b>one</b></u><em><u><i>two</i></u></em>');
+        expect(apply(b)).toEqual('<i>two</i>');
+        expect(apply(c)).toEqual('<em><u><i>two</i></u></em>');
+    });
+});
---
+++ src/SpannedString.ts
@@ -0,0 +1,214 @@
+/**
+ * A string whose substrings can be marked by arbitrary objects.
+ *
+ * The string can be iterated over to get substrings with the list of objects
+ * applied to them, in the order they were applied.
+ */
+type Span<T> = {
+    id: number;
+    attribute: T;
+    isStart: boolean;
+};
+
+export class SpannedString<T> {
+    private _string: string = '';
+    private _spanMarkers: (Span<T>[] | undefined)[] = [undefined];
+    private _nextId: number = 0;
+
+    constructor(
+        string: string,
+        spans: (Span<T>[] | undefined)[],
+        nextId: number
+    ) {
+        this._string = string;
+        this._spanMarkers = spans;
+        this._nextId = nextId;
+    }
+
+    static create<T>() {
+        return new SpannedString<T>('', [undefined], 0);
+    }
+
+    addSpan(startIndex: number, endIndex: number, attribute: T) {
+        this._spanMarkers[startIndex] = this._spanMarkers[startIndex] ?? [];
+        this._spanMarkers[startIndex]!.push({
+            id: this._nextId,
+            attribute,
+            isStart: true,
+        });
+
+        this._spanMarkers[endIndex] = this._spanMarkers[endIndex] ?? [];
+        this._spanMarkers[endIndex]!.push({
+            id: this._nextId,
+            attribute,
+            isStart: false,
+        });
+
+        this._nextId++;
+
+        return this;
+    }
+
+    appendString(string: string, ...attributes: T[]): SpannedString<T> {
+        const startIndex = this._string.length;
+        const endIndex = startIndex + string.length;
+
+        this._string += string;
+        this._spanMarkers = this._spanMarkers.concat(new Array(string.length));
+
+        for (const attribute of attributes) {
+            this.addSpan(startIndex, endIndex, attribute);
+        }
+
+        return this;
+    }
+
+    appendSpannedString(other: SpannedString<T>): SpannedString<T> {
+        this._string = this._string.concat(other._string);
+
+        // The trailing span marker of this string and leading span marker of
+        // the other string will overlap, so we need to merge it
+        const spanMarkers = this._spanMarkers.concat(
+            new Array(other._spanMarkers.length - 1)
+        );
+        const overlappingSpanIndex = this._spanMarkers.length - 1;
+        for (
+            let otherIndex = 0;
+            otherIndex < other._spanMarkers.length;
+            otherIndex++
+        ) {
+            const otherSpans = other._spanMarkers[otherIndex];
+            if (otherSpans) {
+                const index = otherIndex + overlappingSpanIndex;
+                spanMarkers[index] = spanMarkers[index] ?? [];
+                spanMarkers[index]?.push(...otherSpans);
+            }
+        }
+        this._spanMarkers = spanMarkers;
+
+        // We don't need to remap the ids of the spans, because they only occur
+        // together in one place, `overlappingSpanIndex` and at that index, all
+        // the spans of the first string must be closing and all the spans of
+        // the second string must be opening.
+        this._nextId = Math.max(this._nextId, other._nextId);
+
+        return this;
+    }
+
+    slice(
+        startIndex: number,
+        endIndex: number = this._string.length
+    ): SpannedString<T> {
+        if (startIndex < 0 || endIndex < 0) {
+            throw new Error('Invalid start or end index');
+        }
+        if (startIndex > this._string.length) {
+            startIndex = this._string.length;
+        }
+        if (endIndex > this._string.length) {
+            endIndex = this._string.length;
+        }
+
+        const spanMarkers = new Array(endIndex + 1 - startIndex);
+        const activeSpansById = new Map<number, Span<T>>();
+        for (let index = 0; index < this._spanMarkers.length; index++) {
+            const sliceIndex = index - startIndex;
+
+            if (sliceIndex === spanMarkers.length - 1) {
+                spanMarkers[spanMarkers.length - 1] = Array.from(
+                    activeSpansById.values()
+                ).map(({ id, attribute }) => ({
+                    id,
+                    attribute,
+                    isStart: false,
+                }));
+            }
+
+            const spans = this._spanMarkers[index];
+            if (spans) {
+                for (const span of spans) {
+                    if (span.isStart) {
+                        activeSpansById.set(span.id, span);
+                    } else {
+                        activeSpansById.delete(span.id);
+                    }
+                }
+                if (sliceIndex >= 0 && sliceIndex < spanMarkers.length) {
+                    spanMarkers[sliceIndex] = spanMarkers[sliceIndex] ?? [];
+                    spanMarkers[sliceIndex].push(...spans);
+                }
+            }
+
+            if (sliceIndex === 0) {
+                spanMarkers[0] = Array.from(activeSpansById.values()).map(
+                    ({ id, attribute }) => ({
+                        id,
+                        attribute,
+                        isStart: true,
+                    })
+                );
+            }
+        }
+
+        return new SpannedString<T>(
+            this._string.slice(startIndex, endIndex),
+            spanMarkers,
+            this._nextId
+        );
+    }
+
+    padEnd(maxLength: number, fillString?: string) {
+        const paddingLength = maxLength - this._string.length;
+        if (paddingLength > 0) {
+            this.appendString(''.padEnd(paddingLength, fillString));
+        }
+        return this;
+    }
+
+    getString(): string {
+        return this._string;
+    }
+
+    *iterSubstrings(): IterableIterator<[string, T[]]> {
+        let lastIndex = 0;
+        let activeSpans: Span<T>[] = [];
+        for (let spanIndex = 0; spanIndex <= this._string.length; spanIndex++) {
+            const spans = this._spanMarkers[spanIndex];
+
+            if (spans === undefined || spans.length === 0) {
+                continue;
+            }
+
+            if (spanIndex > lastIndex) {
+                activeSpans.sort((a, b) => a.id - b.id);
+                yield [
+                    this._string.slice(lastIndex, spanIndex),
+                    activeSpans.map((span) => span.attribute),
+                ];
+            }
+
+            const toRemove = new Set(
+                spans
+                    .filter((span) => !span.isStart)
+                    .map((span) => span.attribute)
+            );
+            activeSpans = activeSpans.filter(
+                (span) => !toRemove.has(span.attribute)
+            );
+            for (const span of spans) {
+                if (span.isStart) {
+                    activeSpans.push(span);
+                }
+            }
+
+            lastIndex = spanIndex;
+        }
+
+        if (lastIndex < this._string.length) {
+            yield [
+                this._string.slice(lastIndex),
+                activeSpans.map((span) => span.attribute),
+            ];
+        }
+    }
+}
