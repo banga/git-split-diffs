banga/git-split-diffs

commit ba9ccc38e9ba33cb9b2125055fa16375aa2017f2

Update shiki version

src/context.ts
@@ -34,6 +34,7 @@ export async function getContextForConfig(config: Config): Promise<Context> {
     if (config.SYNTAX_HIGHLIGHTING_THEME) {
         HIGHLIGHTER = await getHighlighter({
             theme: config.SYNTAX_HIGHLIGHTING_THEME,
+            langs: ['git-commit'],
         });
     }
 

src/highlightSyntaxInLine.ts
@@ -1,8 +1,10 @@
 import path from 'path';
-import { Highlighter, IThemedToken } from 'shiki';
+import { BUNDLED_LANGUAGES, Highlighter, IThemedToken, Lang } from 'shiki';
 import { FormattedString } from './formattedString';
 import { parseColorDefinition, ThemeColor } from './themes';
 export type HighlightedText = [string, ThemeColor | null];
+
+const _seenLanguages = new Set<string>();
 
 export function highlightSyntaxInLine(
     line: FormattedString,
@@ -13,6 +15,12 @@ export function highlightSyntaxInLine(
         return;
     }
     const language = path.extname(fileName).slice(1);
+    if (!_seenLanguages.has(language)) {
+        if (BUNDLED_LANGUAGES.some((l) => l.id === language)) {
+            highlighter.loadLanguage(language as Lang);
+        }
+        _seenLanguages.add(language);
+    }
 
     let tokens: IThemedToken[];
     try {

commit 0d1f0793a7a9ad9df12a8b3a3d22628d79a49221

Handle highlighter language loading correctly
WARNING: This requires https://github.com/shikijs/shiki/issues/438 to be fixed

https://github.com/banga/git-split-diffs/commit/ba9ccc38e9ba33cb9b2125055fa16375aa2017f2
broke syntax highlighting. It added the `langs` field to
`getHighlighter` which is great for speeding up start up time, but
requires loading languages manually, which is a bit fragile.

There were two issues with the previous implementation:
1. In order to load a language, we were only checking for lang.id in
   `BUNDLED_LANGUAGES`. We should've also looked at the `aliases`, since
   file extensions are usually a better match for those.
2. We were not `await`-ing the `loadLanguage` call, which meant that the
   `highlighter` call would break if the language wasn't already loaded
   (due to a previous loadLanguage invocation).

I've fixed both of these issues and added a snapshot test for
highlighting (which just verifies that .md and .ts files are
highlighted).

Also added TODOs to catch unawait-ed async calls and benchmark syntax
highlighting performance.

src/context.ts
@@ -34,8 +34,7 @@ export async function getContextForConfig(config: Config): Promise<Context> {
     if (config.SYNTAX_HIGHLIGHTING_THEME) {
         HIGHLIGHTER = await shiki.getHighlighter({
             theme: config.SYNTAX_HIGHLIGHTING_THEME,
-            // Load all languages, until https://github.com/shikijs/shiki/issues/438 is fixed
-            // langs: ['git-commit'],
+            langs: ['git-commit'],
         });
     }
 

src/formatAndFitHunkLine.ts
@@ -9,13 +9,13 @@ import { ThemeColor } from './themes';
 // Assuming people aren't editing lines >=100k lines
 const LINE_NUMBER_WIDTH = 5;
 
-export function* formatAndFitHunkLine(
+export async function* formatAndFitHunkLine(
     context: Context,
     fileName: string,
     lineNo: number,
     line: string | null,
     changes: Change[] | null
-): Iterable<FormattedString> {
+): AsyncIterable<FormattedString> {
     const {
         BLANK_LINE,
         LINE_WIDTH,
@@ -66,7 +66,7 @@ export function* formatAndFitHunkLine(
     let isFirstLine = true;
     const formattedLine = T().appendString(lineText);
     highlightChangesInLine(context, linePrefix, formattedLine, changes);
-    highlightSyntaxInLine(formattedLine, fileName, context.HIGHLIGHTER);
+    await highlightSyntaxInLine(formattedLine, fileName, context.HIGHLIGHTER);
 
     for (const fittedLine of iterFitTextToWidth(
         context,

src/highlightSyntaxInLine.ts
@@ -4,28 +4,51 @@ import { FormattedString } from './formattedString';
 import { parseColorDefinition, ThemeColor } from './themes';
 export type HighlightedText = [string, ThemeColor | null];
 
-export function highlightSyntaxInLine(
+async function _loadHighlighterLanguage(
+    highlighter: shiki.Highlighter,
+    fileExtension: string
+): Promise<shiki.Lang | null> {
+    const language = fileExtension as shiki.Lang;
+
+    // Skip if already loaded
+    if (highlighter.getLoadedLanguages().includes(language)) {
+        return language;
+    }
+
+    // If supported, load it and return
+    const hasLanguage = shiki.BUNDLED_LANGUAGES.some(
+        (l) => l.id === fileExtension || l.aliases?.includes(fileExtension)
+    );
+    if (hasLanguage) {
+        await highlighter.loadLanguage(language);
+        return language;
+    }
+
+    // Unsupported language
+    return null;
+}
+
+export async function highlightSyntaxInLine(
     line: FormattedString,
     fileName: string,
     highlighter?: shiki.Highlighter
-): void {
+): Promise<void> {
     if (!highlighter) {
         return;
     }
-    const language = path.extname(fileName).slice(1);
 
-    let tokens: shiki.IThemedToken[];
-    try {
-        [tokens] = highlighter.codeToThemedTokens(
-            line.getString(),
-            language,
-            undefined,
-            { includeExplanation: false }
-        );
-    } catch (e) {
-        // Highlighting fails if a language grammar or theme is missing
+    const fileExtension = path.extname(fileName).slice(1);
+    const language = await _loadHighlighterLanguage(highlighter, fileExtension);
+    if (!language) {
         return;
     }
+
+    const [tokens] = highlighter.codeToThemedTokens(
+        line.getString(),
+        language,
+        undefined,
+        { includeExplanation: false }
+    );
 
     let index = 0;
     for (const { content, color } of tokens) {

src/iterFormatHunk.ts
@@ -5,8 +5,9 @@ import { T, FormattedString } from './formattedString';
 import { getChangesInLines } from './highlightChangesInLine';
 import { iterFitTextToWidth } from './iterFitTextToWidth';
 import { zip } from './zip';
+import { zipAsync } from './zipAsync';
 
-function* iterFormatHunkSplit(
+async function* iterFormatHunkSplit(
     context: Context,
     fileNameA: string,
     fileNameB: string,
@@ -15,7 +16,7 @@ function* iterFormatHunkSplit(
     lineNoA: number,
     lineNoB: number,
     lineChanges: (Change[] | null)[]
-): Iterable<FormattedString> {
+): AsyncIterable<FormattedString> {
     const { MISSING_LINE_COLOR, BLANK_LINE } = context;
 
     for (const [lineA, lineB, changes] of zip(
@@ -40,7 +41,7 @@ function* iterFormatHunkSplit(
 
         const missingLine = T().appendString(BLANK_LINE, MISSING_LINE_COLOR);
 
-        for (const [formattedLineA, formattedLineB] of zip(
+        for await (const [formattedLineA, formattedLineB] of zipAsync(
             formattedLinesA,
             formattedLinesB
         )) {
@@ -58,7 +59,7 @@ function* iterFormatHunkSplit(
     }
 }
 
-function* iterFormatHunkUnified(
+async function* iterFormatHunkUnified(
     context: Context,
     fileNameA: string,
     fileNameB: string,
@@ -67,7 +68,7 @@ function* iterFormatHunkUnified(
     lineNoA: number,
     lineNoB: number,
     lineChanges: (Change[] | null)[]
-): Iterable<FormattedString> {
+): AsyncIterable<FormattedString> {
     for (let indexA = 0, indexB = 0; indexA < hunkLinesA.length; indexA++) {
         const hunkLineA = hunkLinesA[indexA];
         const prefixA = hunkLineA?.slice(0, 1) ?? null;
@@ -118,7 +119,7 @@ function* iterFormatHunkUnified(
     }
 }
 
-export function* iterFormatHunk(
+export async function* iterFormatHunk(
     context: Context,
     hunkHeaderLine: string,
     fileNameA: string,
@@ -127,7 +128,7 @@ export function* iterFormatHunk(
     hunkLinesB: (string | null)[],
     lineNoA: number,
     lineNoB: number
-): Iterable<FormattedString> {
+): AsyncIterable<FormattedString> {
     const { HUNK_HEADER_COLOR, SCREEN_WIDTH, SPLIT_DIFFS } = context;
 
     yield* iterFitTextToWidth(

src/iterSideBySideDiffs.ts
@@ -50,7 +50,7 @@ async function* iterSideBySideDiffsFormatted(
     let hunkHeaderLine: string = '';
     let hunkLinesA: (string | null)[] = [];
     let hunkLinesB: (string | null)[] = [];
-    function* yieldHunk() {
+    async function* yieldHunk() {
         yield* iterFormatHunk(
             context,
             hunkHeaderLine,
@@ -65,7 +65,7 @@ async function* iterSideBySideDiffsFormatted(
         hunkLinesB = [];
     }
 
-    function* flushPending() {
+    async function* flushPending() {
         if (state === 'diff') {
             yield* yieldFileName();
         } else if (state === 'hunk-body') {

src/zipAsync.ts
@@ -1,0 +1,21 @@
+export async function* zipAsync<T extends Array<any>>(
+    ...iterables: { [K in keyof T]: AsyncIterable<T[K]> }
+): AsyncIterable<{ [K in keyof T]: T[K] | undefined }> {
+    const iterators = iterables.map((iterable) =>
+        iterable[Symbol.asyncIterator]()
+    );
+
+    while (true) {
+        const values = [];
+        let hasMore = false;
+        for (const iterator of iterators) {
+            const { done, value } = await iterator.next();
+            hasMore ||= !done;
+            values.push(value);
+        }
+        if (!hasMore) {
+            return;
+        }
+        yield values as T;
+    }
+}

src/zipAsync.test.ts
@@ -1,0 +1,68 @@
+import { zipAsync } from './zipAsync';
+
+async function collectAsync(...params: Parameters<typeof zipAsync>) {
+    let items = [];
+    for await (const item of zipAsync(...params)) {
+        items.push(item);
+    }
+    return items;
+}
+
+test('empty', async () => {
+    expect(await collectAsync(zipAsync())).toEqual([]);
+});
+
+test('single', async () => {
+    async function* gen() {
+        yield 1;
+        yield 2;
+        yield 3;
+    }
+
+    expect(await collectAsync(gen())).toEqual([[1], [2], [3]]);
+});
+
+test('equal lengths', async () => {
+    async function* f() {
+        yield 1;
+        yield 2;
+        yield 3;
+    }
+
+    async function* g() {
+        yield 10;
+        yield 20;
+        yield 30;
+    }
+
+    expect(await collectAsync(f(), g())).toEqual([
+        [1, 10],
+        [2, 20],
+        [3, 30],
+    ]);
+});
+
+test('unequal lengths', async () => {
+    async function* f() {
+        yield 1;
+        yield 2;
+    }
+
+    async function* g() {
+        yield 10;
+        yield 20;
+        yield 30;
+    }
+
+    expect(await collectAsync(f(), g())).toEqual([
+        [1, 10],
+        [2, 20],
+        [undefined, 30],
+    ]);
+
+    expect(await collectAsync(g(), f())).toEqual([
+        [10, 1],
+        [20, 2],
+        [30, undefined],
+    ]);
+});