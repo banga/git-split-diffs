fc8b091d70372e82183bbc720112b3c6d074b7ec
Shrey Banga <banga.shrey@gmail.com>
2023-12-29 00:02:46 -0500

    Revive generateScreenshots.sh

--- scripts/generateScreenshots.sh
+++ scripts/generateScreenshots.sh
@@ -1,14 +1,18 @@
-rows=$(tput lines)
-window_id=$(osascript -e 'tell app "iTerm" to id of window 1') 
+#!/bin/sh
 
-for theme in "$@"
-do
+set -e
+
+rows=$(tput lines)
+window_id=$(osascript -e 'tell app "iTerm" to id of window 1')
+
+npm run build:publish
+
+for theme in "$@"; do
     git config --replace-all split-diffs.theme-name "$theme"
     clear
-    git --no-pager show 5a00d16095a255b57c762289fa434e18088b956b src/iterSideBySideDiffs.ts | ./bin/git-split-diffs --color | head -n $(($rows-1))
-    screencapture -x -T 1 -l $window_id "screenshots/$theme.png"
+    git --no-pager show 5a00d16095a255b57c762289fa434e18088b956b src/iterSideBySideDiffs.ts | node ./build/index.mjs --color | head -n $((rows - 1))
+    screencapture -x -T 1 -l "$window_id" "screenshots/$theme.png"
     open -a ImageOptim "screenshots/$theme.png"
 done
 
 git config --unset split-diffs.theme-name
-
